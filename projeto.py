46e89ad2bff3eb30ed95c78f06a58ba48492ec65
Marcos Queiroz <marcosamfq123@gmail.com>
2024-08-20 18:58:09 -0300

    alterado projeto e adicionado exemplos

--- projeto.py
+++ projeto.py
@@ -1,24 +1,31 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,jsonify
 from pyswip import Prolog
 
 app = Flask(__name__)
 
-
 prolog = Prolog()
 prolog.consult("prolog_db.pl")
 
+#testes
+print(list(prolog.query("filme('The Dark Knight', Diretor, Genero, Ano)")))
+
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def home_page():
     resultado = None
     if request.method == 'POST':
         consulta = request.form['consulta']
+        print(type({consulta}))
+        print(consulta)
 
-        resultado = prolog.query(consulta)
-
-        for x in resultado:
-            print(f'resultado: {x}')
-        
+        resultado = consulta
+        #resultado = list(prolog.query(consulta))
+       
     return render_template("home_page.html", resultado=resultado)
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
